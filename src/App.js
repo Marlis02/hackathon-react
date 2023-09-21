38c979e34fd962d8571b3afaf07346fb9d45f802
Marlis02 <erkinbekovmarlis70@gmail.com>
2023-09-21 19:00:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import IndividualIntervalsExample from "./Carusel";
+import Carusel from "./Carusel";
+import Hav from "./navbar";
+// const API = "http://localhost:3001";
+
+// const res = axios.get(API); // Указываем полный URL, включая протокол (https://)
+// console.log(res); // Выводим данные из ответа
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hav />
+      <IndividualIntervalsExample />
+      dsllfjlkdjl
     </div>
   );
 }
