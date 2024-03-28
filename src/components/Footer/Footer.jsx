1cc231870613a62cd43ccaa26b429984c6a598b8
Marlis02 <erkinbekovmarlis70@gmail.com>
2024-03-29 01:25:50 +0600

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import "./footer.css";
-import { useNavigate } from "react-router-dom";
-import { BsLinkedin } from "react-icons/bs";
-import { FiInstagram } from "react-icons/fi";
-import { AiFillTwitterCircle } from "react-icons/ai";
-import { BsGithub } from "react-icons/bs";
+import React from 'react'
+import './footer.css'
+import { useNavigate } from 'react-router-dom'
+import { BsLinkedin } from 'react-icons/bs'
+import { FiInstagram } from 'react-icons/fi'
+import { AiFillTwitterCircle } from 'react-icons/ai'
+import { BsGithub } from 'react-icons/bs'
 
 const Footer = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   return (
     <footer
       className="page-footer font-small blue pt-4"
-      style={{ height: "auto", marginTop: "50px" }}
+      style={{ height: 'auto', marginTop: '50px' }}
     >
       <div
         className="container-fluid text-center text-md-left"
-        style={{ height: "350px" }}
+        style={{ height: '350px' }}
       >
         <div className="row">
           <div className="col-md-6 mt-md-0 mt-3">
@@ -35,22 +35,22 @@ const Footer = () => {
             <h5 className="text-uppercase footer-text">Products</h5>
             <ul className="list-unstyled">
               <li>
-                <p className="links" onClick={() => navigate("/*")}>
+                <p className="links" onClick={() => navigate('/*')}>
                   Home
                 </p>
               </li>
               <li>
-                <p className="links" onClick={() => navigate("/products")}>
+                <p className="links" onClick={() => navigate('/products')}>
                   Products
                 </p>
               </li>
               <li>
-                <p className="links" onClick={() => navigate("/about")}>
+                <p className="links" onClick={() => navigate('/about')}>
                   About us
                 </p>
               </li>
               <li>
-                <p className="links" onClick={() => navigate("/categories")}>
+                <p className="links" onClick={() => navigate('/categories')}>
                   Categories
                 </p>
               </li>
@@ -88,18 +88,18 @@ const Footer = () => {
       <div
         className="footer-copyright text-center py-3 footer-text"
         style={{
-          width: "100%",
-          height: "100px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          borderTop: "1px solid white",
+          width: '100%',
+          height: '100px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderTop: '1px solid white',
         }}
       >
         © 2023 Apple Store
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
